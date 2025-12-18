d218a4865c2ea3d2da88115aa439aefe3a99628b
dxcmint868 <nddminh2021@gmail.com>
2025-12-18 20:21:05 +0700

    feat: implement Clerk webhook verification using verifyWebhook function

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { Logger } from "@nestjs/common";
+import * as bodyParser from "body-parser";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         logger: new Logger(),
+        bodyParser: false,
     });
+
+    app.use("/clerk-webhook", bodyParser.raw({ type: "application/json" }));
     await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
